c1c389f6f57aa938ef1aaf2f33cb96a54cc70f88
pgm-brentvansteertegem <brent.vansteertegem@student.arteveldehs.be>
2022-01-26 11:36:37 +0100

    Menu added to header

--- static/js/general.js
+++ static/js/general.js
@@ -1,5 +1,27 @@
 const init = () => {
+    cacheHeaderElements();
+    registerListeners();
     randomizeHeader();
+}
+
+const cacheHeaderElements = () => {
+  this.$menu = document.querySelector('#menu');
+  this.$toggleMenuButton = document.querySelector('#toggle_menu');
+  this.$toggleMenuImage = document.querySelector('#toggle_menu_img');
+}
+
+const registerListeners = () => {
+  this.$toggleMenuButton.addEventListener('click', () => {
+    if (!this.$menu.classList.contains('opened')){
+      this.$menu.classList.add('opened');
+      this.$toggleMenuImage.src = 'static/media/img/icons/cross.svg';
+      document.body.classList.add('menu_opened');
+    } else {
+      this.$menu.classList.remove('opened');
+      this.$toggleMenuImage.src = 'static/media/img/icons/hamburger.svg';
+      document.body.classList.remove('menu_opened');
+    }
+  });
 }
 
 const randomizeHeader = () => {
